3db424d41baafcdeb7a9be857740d31e79a120a1
Mikaeel92 <mikaeelyaghoobzadeh@gmail.com>
2024-12-26 14:40:17 +0330

    done!

--- react-accordion/src/App.jsx
+++ react-accordion/src/App.jsx
@@ -3,15 +3,21 @@ import data from './assets/data.js'
 
 const App = () => {
 
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState([])
+
+  const handleClick = (currentItem) => {
+    let copyState = [...show]
+    copyState.includes(currentItem) ? copyState = copyState.filter((item) => item != currentItem) : copyState.push(currentItem)
+    setShow(copyState)
+  }
 
   return (
     <div className='flex flex-col h-screen w-[1000px] items-center justify-center mx-auto'>
       {
         data.map((item) => (
           <div className='flex flex-col items-center justify-center p-4 mx-auto text-center gap-4 tracking-wide' key={item.id}>
-            <h1 className='text-blue-800 font-bold text-2xl'>{item.question}<span className='text-gray-700 mx-4'>+</span></h1>
-            <p>{item.answer}</p>
+            <h1 className='text-blue-800 font-bold text-2xl'>{item.question}<span onClick={() => handleClick(item.id)} className='text-gray-700 mx-4'>{show.includes(item.id) ? '-' : '+'}</span></h1>
+            <p>{show.includes(item.id) ? item.answer : null}</p>
           </div>
         ))
       }
